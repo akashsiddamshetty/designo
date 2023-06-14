abb1f546e55b6e4a4ec9d5021a8fd74c3497744d
AKASH SIDDAMSHETTY <akashsiddamshetty@gmail.com>
2023-06-14 22:56:58 +0530

    feat - our-company lacations contact pages added

---
+++ components/locations/page.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import Header from '../Header';
+import Footer from '../Footer';
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({  }) => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default page;
